6a098223c73fce1ddada38188375b37f2a6d88b0
kartik pal <kartik.pal@visdum.com>
2024-10-06 17:26:24 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+
+
+import { ReactLenis, useLenis } from 'lenis/react'
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useGSAP } from '@gsap/react';
+
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
+
+
+
 // Components
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Review from "./components/Review";
@@ -8,8 +21,30 @@ import Skill from "./components/Skill";
 import Work from "./components/Work";
 
 const App = () => {
+
+  useGSAP(() => {
+    const elements = gsap.utils.toArray('.reveal-up');
+    elements.forEach((element) => {
+      gsap.to(element, {
+        scrollTrigger: {
+          trigger: element,
+          start:'-200 bottom',
+          end: 'bottom 80%',
+          scrub: true,
+        },
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: 'power2.out'
+      })
+    })
+
+    console.log(elements);
+    
+  });
+
   return (
-    <>
+    <ReactLenis root>
       <Header />
       <main>
         <Hero />
@@ -19,7 +54,8 @@ const App = () => {
         <Review />
         <Contact />
       </main>
-    </>
+      <Footer />
+      </ReactLenis>
   );
 };
 
